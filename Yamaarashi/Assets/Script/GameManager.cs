682b4d8bd36f8d4e845d86c97d6bf9f973614001
fujita <t>
2017-11-14 14:28:01 +0900

    11/14

---
+++ Yamaarashi/Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Text scoreText;
+    public static int score = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = score.ToString();
+
+    }
+}
---
+++ Yamaarashi/Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Vector3 vel = new Vector3(0.0f, 0.0f, 0.0f);
+    public float speed = 1.0f;
+
+    public Rigidbody rb;
+    public Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
+
+    private Vector3 zero = new Vector3(0.0f, 0.0f, 0.0f);
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        velocity.z -= 0.9f;
+        if (velocity.z <= 0)
+        {
+            velocity.z = 0.0f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.z = speed*10.0f;
+        }
+        rb.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+}
+
