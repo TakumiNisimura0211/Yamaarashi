ff1c6e73df889758e370798fce8ddb695fffdb0b
fujita <t>
2017-11-21 14:26:14 +0900

    11/21 14:26

---
+++ Yamaarashi/Assets/Script/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    Rigidbody rb;
+    public float JumpPower = 250;
+    public bool jump = false;
+    GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag== "Player")
+        {
+            if(jump)
+            {
+                player.GetComponent<Rigidbody>().AddForce(transform.up * JumpPower * Time.deltaTime, ForceMode.Impulse);
+            }
+        }
+    }
+
+}
