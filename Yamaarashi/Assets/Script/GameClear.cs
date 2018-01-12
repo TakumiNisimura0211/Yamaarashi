27d73004f70ff2301e2ff497d82e9ecbecce210e
西村拓海 <kbc15b11@stu.kawahara.ac.jp>
2018-01-12 13:24:42 +0900

    GameClear

---
+++ Yamaarashi/Assets/Script/GameClear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("EndScene");
+        }
+    }
+}
