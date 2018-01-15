7a5ca0c83a51633fb5d69757d3fef4885aa17340
西村拓海 <kbc15b11@stu.kawahara.ac.jp>
2018-01-15 14:29:00 +0900

    Time

---
+++ Yamaarashi/Assets/Script/TimeText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeText : MonoBehaviour {
+
+    int minute;
+    float second;
+    //前のUpdate時の秒数
+    float oldsecond;
+    //表示用Text
+    Text timetext;
+
+	// Use this for initialization
+	void Start () {
+        minute = 0;
+        second = 0f;
+        oldsecond = 0f;
+        timetext = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        second += Time.deltaTime;
+        if(second >= 60.0f)
+        {
+            minute++;
+            second = second - 60.0f;
+        }
+        //値が切り替わった時だけ更新
+        if((int)second != (int)oldsecond)
+        {
+            timetext.text = "Time " + minute.ToString("00") + ":" + ((int)second).ToString("00");
+        }
+        oldsecond = second;
+	}
+}
